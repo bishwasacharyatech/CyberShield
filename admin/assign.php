0ee9cfeccb6d17db562d14804b639f0196655657
bishwasacharyatech <bishwasacharya.tech@gmail.com>
2026-09-23 18:41:56 +0545

    add print button on report page

--- admin/assign.php
+++ admin/assign.php
@@ -273,4 +273,8 @@ sidebar('admin', 'assign');
     </form>
 </div>
 
+<div style="text-align:center;margin-top:20px">
+    <a href="<?= BASE_URL ?>/export-single-report.php?id=<?= $id ?>" class="btn btn-gr">⬇ Download Report as pdf</a>
+</div>
+
 <?php pageEnd(); ?>
